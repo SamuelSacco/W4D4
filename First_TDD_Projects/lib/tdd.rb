d119f5316afe748faa7cf22cd5e02996845d1e1a
taihuynh812 <taihuynh@revlisoft.com>
2021-05-13 14:12:20 -0400

    completed some rspec problems

--- First_TDD_Projects/lib/tdd.rb
+++ First_TDD_Projects/lib/tdd.rb
@@ -4,4 +4,36 @@ class Array
         self.each {|el| output << el if !output.include?(el)}
         output
     end
+
+    def two_sum
+        indices = []
+        (0...self.length).each do |i|
+            (i+1...self.length).each do |j|
+                indices << [i, j] if self[i] + self[j] == 0     
+            end
+        end
+        indices
+    end
+    
+end
+
+def my_transpose(arr)
+    arr.transpose     
+end
+
+arr = %w(10 7 5 11 2 5 6 10).map(&:to_i)
+
+def stock_pick(arr)
+    pair = []
+    max_diff = 0
+    (0...arr.length).each do |i|
+        (i+1...arr.length).each do |j|
+            diff = arr[j] - arr[i] 
+            if diff > max_diff
+                pair = [arr[i],arr[j]] 
+                max_diff = diff
+            end
+        end
+    end
+    pair
 end
--- First_TDD_Projects/spec/tdd_spec.rb
+++ First_TDD_Projects/spec/tdd_spec.rb
@@ -6,4 +6,34 @@ describe "Array" do
             expect([1,2,1,3,3].my_uniq).to eq([1, 2, 3])
         end
     end
+
+    describe "#two_sum" do
+        it "return all pair of indices where elements sum to zero" do
+            expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+        end
+
+        it "output should be sorted" do
+            expect([-1, 0, 2, -2, 1].two_sum).to_not eq([[2, 3],[0, 4]])
+        end
+    end
+
+    
+end
+
+describe "my_transpose" do
+    it "convert between the row-oriented and column-oriented representations" do
+        expect(my_transpose([[0, 1, 2],[3, 4, 5],[6, 7, 8]])).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+    end
+end
+
+describe "stock_pick" do
+    it "outputs the most profitable pair of days" do
+        arr = %w(10 7 5 11 2 5 6 10).map(&:to_i)
+        expect(stock_pick(arr)).to eq([2, 10])
+    end
+    
+    it "you can't sell stock before you buy it" do
+        arr = %w(10 7 5 11 2 5 6 10).map(&:to_i)
+        expect(stock_pick(arr)).to_not eq([11, 2])
+    end
 end
